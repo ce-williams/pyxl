ad115cf62f7e611b215b5bf0e32bd8da42ebb646
cewilliams <cewilliams@csbsju.edu>
2019-04-18 01:07:24 -0500

    create and update files

---
+++ create.py
@@ -0,0 +1,30 @@
+from openpyxl import Workbook
+from openpyxl.utils import get_column_letter
+
+
+import datetime
+date = datetime.datetime.now()
+
+wb = Workbook()
+# creates new file with timestamp of date withing filename
+dest_filename = f'empty_book_{date}.xlsx'
+
+ws1 = wb.active
+ws1.title = "range names"
+
+
+for row in range(1, 40):
+    ws1.append(range(600))
+
+ws2 = wb.create_sheet(title="Pi")
+
+ws2['F5'] = 3.14
+
+ws3 = wb.create_sheet(title="Data")
+for row in range(10, 20):
+    for col in range(27, 54):
+         _ = ws3.cell(column=col, row=row, value="{0}".format(get_column_letter(col)))
+print(ws3['AA10'].value)
+
+
+wb.save(filename = dest_filename)
